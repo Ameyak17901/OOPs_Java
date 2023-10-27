437a9deaadfb96e5c6ac845c67620f79d639046e
Ameya Kulkarni <ameyak2001@gmail.com>
2023-10-27 00:37:59 -0700

    Student class with id autogenerated

---
+++ Student.java
@@ -0,0 +1,71 @@
+
+public class Student {
+	private String id;
+	private String name;
+	private int m1,m2,m3;
+	static int count;
+	
+	static {
+		count = 0;
+	}
+	
+	private String generateID() {
+		
+		return name.substring(0,1)+count;			
+	}
+	public Student() {
+		count++;
+		id = "";
+		name = "User";
+		m1=0;
+		m2=0;
+		m3=0;
+		
+	}
+	
+	public Student(String name, int m1,int m2,int m3) {
+		if(name != null) {
+			this.name = name;
+		}
+			this.id = generateID();
+			this.m1=m1;
+			this.m2=m2;
+			this.m3=m3;
+			count++;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getM1() {
+		return m1;
+	}
+	public int getM2() {
+		return m2;
+	}
+	public int getM3() {
+		return m3;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	public void setM1(int m) {
+		m1 = m;
+	}
+	public void setM2(int m) {
+		m2 = m;
+	}
+	public void setM3(int m) {
+		m3 = m;
+	}
+	
+	public String toString() {
+		return "ID : "+this.id+"\nName : "+ this.name + "\nM1 : "+this.m1+ "\nM2 : "+this.m2+ "\nM3 : "+this.m3;
+	}
+}
---
+++ TestStudent.java
@@ -0,0 +1,15 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		Student s1 = new Student("Ameya",80,90,100);
+		System.out.println(s1);
+		System.out.println();
+		Student s2 = new Student("Rohit",40,60,10);
+		System.out.println(s2);
+		System.out.println();
+		Student s3 = new Student("Rohan",49,65,18);
+		System.out.println(s3);
+	}
+
+}
